7545d798ca2a0b3a27d4af4ed850f29bb9151cb6
Hrithic <hrithicraj730@gmail.com>
2025-02-05 18:43:13 +0530

    cors updated with methods

--- src/app.ts
+++ src/app.ts
@@ -45,8 +45,9 @@ app.use(
   cors({
       // origin: 'http://localhost:5173',
       origin: ['https://filmista-frontend.vercel.app'],
-      credentials: true,
+      methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
       allowedHeaders: "Content-Type, Authorization",
+      credentials: true,
   })
 );
 
