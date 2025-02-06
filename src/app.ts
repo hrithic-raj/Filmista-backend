da0a754f5896cb06a0fc4824126800989a5ef6ab
Hrithic <hrithicraj730@gmail.com>
2025-02-06 18:27:24 +0530

    front end url for cors updated

--- src/app.ts
+++ src/app.ts
@@ -44,7 +44,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
       // origin: 'http://localhost:5173',
-      origin: 'https://filmista-frontend-63bkexaxj-hrithic-rajs-projects.vercel.app',
+      origin: 'https://filmista-frontend.vercel.app',
       methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
       credentials: true,
   })
