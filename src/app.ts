8f28c7be7162c8cc815cc9f4fd3c6d4af82027f9
Hrithic <hrithicraj730@gmail.com>
2025-02-08 13:03:20 +0530

    cors origin updated with netlify url

--- src/app.ts
+++ src/app.ts
@@ -44,7 +44,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
       // origin: 'http://localhost:5173',
-      origin: 'https://filmista-frontend.vercel.app',
+      // origin: 'https://filmista-frontend.vercel.app',
+      origin: 'https://lovely-caramel-5ed216.netlify.app',
       methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
       credentials: true,
   })
