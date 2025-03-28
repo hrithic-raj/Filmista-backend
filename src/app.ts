b028b17f22c614eb13b98c18648beecb9c2192ba
Hrithic <hrithicraj730@gmail.com>
2025-03-28 13:56:46 +0530

    cors updated to send patch methods

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ connectDB()
 // Middleware
 app.use(cors({
   origin: config.FRONTEND_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'PUT'],
   credentials: true,
 }));
 
